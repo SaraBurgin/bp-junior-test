11c5817f813150127ceaa352f5fa1e5c8468cf09
Sara <saragonzalezburgin@hotmail.com>
2020-10-17 13:32:29 +0200

    Added Cryptocurrency data to visualization

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,12 +5,14 @@ import { HttpClient } from '@angular/common/http';
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  inputs: ['items']
+  inputs: ['dataItems', 'cryptoItems']
 })
 export class AppComponent {
   title = 'my-app';
   url = `https://api.bitpanda.com/v1/masterdata`;
-  items = [];
+  cryptoItems = [];
+  dataItems = [];
+
 
   constructor(private http: HttpClient) {
     this.http.get(this.url).toPromise().then(data => {
@@ -26,7 +28,46 @@ export class AppComponent {
         let commodities = val['commodities'];
         let fiats = val['fiats'];
         let indexes = val['indexes'];
-        // console.log(indexes);
+
+        cryptocoins.forEach((cryptocoin) => {
+          let cryptocoinAttributes = cryptocoin['attributes'];
+          let keys = Object.keys(cryptocoinAttributes);
+          let values = Object.values(cryptocoinAttributes);
+
+          let logo;
+          let name;
+          let color;
+          let avgPrice;
+          let changesAmount;
+          let percentage;
+
+          for (var i = 0; i < keys.length; i++) {
+            if (keys[i] === 'logo') {
+              logo = values[i];
+            }
+            if (keys[i] === 'name') {
+              name = values[i];
+            }
+            if (keys[i] === 'color') {
+              color = values[i];
+            }
+            if (keys[i] === 'avg_price') {
+              avgPrice = values[i];
+            }
+            if (keys[i] === 'change_24h_amount') {
+              changesAmount = values[i];
+            }
+            percentage = changesAmount / (avgPrice - changesAmount);
+          }
+          this.cryptoItems.push({
+            logo: logo,
+            name: name,
+            color: color,
+            averagePrice: avgPrice,
+            changes24h: changesAmount,
+            percentage: percentage,
+          })
+        })
 
         indexes.forEach((index) => {
           let indexAttributes = index['attributes'];
@@ -42,33 +83,33 @@ export class AppComponent {
           for (var i = 0; i < keys.length; i++) {
             if (keys[i] === 'logo') {
               logo = values[i];
-              // this.items.push({ logo: logo });
+              // this.dataItems.push({ logo: logo });
             }
             if (keys[i] === 'name') {
               name = values[i];
-              // this.items.push({ name: name });
+              // this.dataItems.push({ name: name });
             }
             if (keys[i] === 'avg_price') {
               avgPrice = values[i];
-              // this.items.push({ averagePrice: avgPrice });
+              // this.dataItems.push({ averagePrice: avgPrice });
             }
             if (keys[i] == 'change_24h_amount') {
               changesAmount = values[i];
-              // this.items.push({ changes24h: changesAmount });
+              // this.dataItems.push({ changes24h: changesAmount });
             };
             if (keys[i] == 'index_original_fiat_symbol') {
               fiatSymbol = values[i];
-              // this.items.push({ currency: fiatSymbol });
+              // this.dataItems.push({ currency: fiatSymbol });
             }
           }
-          this.items.push({
+          this.dataItems.push({
             logo: logo,
             name: name,
             averagePrice: avgPrice,
             changes24h: changesAmount,
             currency: fiatSymbol,
           })
-          console.log(this.items);
+          // console.log(this.dataItems);
         })
       }
     });
@@ -78,4 +119,4 @@ export class AppComponent {
 
 /* The reason to call toPromise is because get returns an observable so there is no real point in observing a get request. You are just getting the data once so you may as well turn it to a promise. */
 
-/* To show the values in JSON in our HTML we create an array items, iterate over every key in data to make sure that that key is actually a real property of the data and push these items to array */
+/* To show the values in JSON in our HTML we create an array dataItems, iterate over every key in data to make sure that that key is actually a real property of the data and push these dataItems to array */
